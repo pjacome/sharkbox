69d9ed0a84e026b9920c5f82356c27c11d7001c0
fmartinez95 <martinezfranciscom@gmail.com>
2016-09-10 22:40:44 -0700

    eventful api example working

---
+++ views/crud.js
@@ -0,0 +1,22 @@
+// EVENTFUL - API KEY : ThPMmx93M7QSk4Mq
+window.onload = function() {
+  var filterButton = document.getElementById("FilterButton");
+  filterButton.onclick = function() {
+    var oArgs = {
+      app_key: "ThPMmx93M7QSk4Mq",
+      q: "music",
+      where: "San Diego",
+      date: "2013061000-2015062000",
+      page_size: "5",
+      sort_order: "popularity"
+    };
+
+    var loc = EVDB.API.call("/events/search", oArgs, function(oData) {
+      var ex = JSON.stringify(oData);
+      var ex2 = document.getElementById("myP");
+      ex2.innerHTML = ex;
+    });
+
+    alert("loc = " + loc);
+  }
+};
